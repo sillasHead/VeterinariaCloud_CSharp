b9e5d69f1216c6ea99eaf452a97051c91efe7142
GUILHERME BORBA DE MELO <guilherme.melo6@fatec.sp.gov.br>
2020-07-29 20:31:19 -0300

    Coloquei a imagem no picturebox

--- VeterinariaCloud_CSharp/App/Form1.cs
+++ VeterinariaCloud_CSharp/App/Form1.cs
@@ -16,5 +16,28 @@ namespace App
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            opdImg.FileName = "imagem";
+            opdImg.Title = "SELECIONE UMA IMAGEM";
+            opdImg.Filter = "JPEG|*.JPG|PNG|*.png";
+            opdImg.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            pictureBox1.Image = Image.FromFile(opdImg.FileName);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
